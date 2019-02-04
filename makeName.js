7940a0ff8f2e09db159a3c484e2d11c3df378e60
Christian <32764514+neowara@users.noreply.github.com>
2019-02-04 23:16:43 +0100

    Created a function expression that can read a file while creating a promise, then I solved that promise and gathered the results from both files in an array of objects and write it into my 3rd file

--- makeName.js
+++ makeName.js
@@ -1,5 +1,30 @@
 var fs = require('fs');
+file = require('fs');
 
-var name = fs.readFileSync('name.txt', 'utf8');
-var lastName = fs.readFileSync('lastName.txt', 'utf8');
+//Created a function expression that reads a given file, this way we can re-use this several times in our code if we want to
+
+let readFile = (filename) =>{
+    return new Promise(function(resolve, reject){
+        file.readFile(filename, 'utf8', function(err, data){
+            if(err){
+                reject(err)
+            }else{
+                resolve(data)
+            }
+        });
+    });
+};
+
+//Here first we solve all of the promises that are created when the files are read
+//then we write the result returned from Promise.all to our fullName.txt file 
+
+Promise.all([
+    readFile('name.txt'),
+    readFile('lastName.txt')
+]).then((results) => {
+    fs.writeFile("fullName.txt", results, (err) => {
+        if (err) throw err;
+        console.log('Full name created');
+    });
+});
 
